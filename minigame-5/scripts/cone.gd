09e28d7cd83a605134b7327f1c8ea9212349f020
pwkkos <jasmineluc90@gmail.com>
2026-04-17 20:40:40 -0700

    update game

--- minigame-5/scripts/cone.gd
+++ minigame-5/scripts/cone.gd
@@ -7,4 +7,4 @@ func _ready() -> void:
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
 	if body is Vehicle:
 		print("player hit cone")
-		
+		%TimeUI._increaseConesHit()
--- minigame-5/scripts/time_ui.gd
+++ minigame-5/scripts/time_ui.gd
@@ -4,7 +4,12 @@ extends VBoxContainer
 @onready var conesText = get_node("ConesText")
 
 var time: float = 0.0
+var cones_hit: int = 0
 
 func _process(delta: float) -> void:
 	time += delta
 	text.text = "time: %.2f" % time
+
+func _increaseConesHit() -> void:
+	cones_hit += 1
+	conesText.text = "cones hit: %s" % cones_hit
